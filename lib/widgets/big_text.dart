eab11ac8a484c12e9e148b6fe24276ec60744c76
Pushpalal-Tharu <pushpalaltharu1@gmail.com>
2023-07-12 23:11:00 +0545

    first commit

---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  TextOverflow overflow;
+
+  BigText({
+    Key? key,
+    this.color = const Color(0xFF332d2b),
+    required this.text,
+    this.size = 20,
+    this.overflow = TextOverflow.ellipsis,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overflow,
+      maxLines: 1,
+      style: TextStyle(
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontFamily: "Roboto",
+        fontSize: size,
+      ),
+    );
+  }
+}
